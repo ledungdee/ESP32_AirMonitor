a5f63cfe08588847fccc77c17b26f9f8334523c4
ledungdee <feelgoood98@gmail.com>
2023-05-30 04:20:38 +0700

    modify web FE, add fireAlert, add AirAlert"

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,9 @@ void handle_NotFound();
 void check_MQ135();
 void check_DHT11();
 void check_PMS();
-String SendHTML(int val1, int val2, int val3);
+String SendHTML(int pm1, int pm25, int pm10, float tempC, float humi);
+
+
 
 #define BUZZER 23
 #define MQ135_D 18
@@ -29,18 +31,16 @@ String SendHTML(int val1, int val2, int val3);
 #define RL2 33
 
 
-//bool ledState = 0;
-//bool buzzerState = 0;
+bool ledState = 0, buzzerState = 0, RL1State = 0;
 bool gasDetect = 0;
-bool alert = 0;
-bool RL1State = 0;
-bool RL2State = 0;
+bool fireAlert = 0, airAlert = 0;
+int pm1State = 0, pm25State = 0, pm10State = 0;
 const char* ssid = "A3a.10";
 const char* password = "888888Aa";
 char default_num[13] = {'+', '8', '4', '9', '6', '7', '9', '9', '2', '2', '9', '8' };
 float humi = 0, tempC = 0, tempF = 0;
 
-struct pms5003data {
+struct pms7003data {
   uint16_t framelen;
   uint16_t pm10_standard, pm25_standard, pm100_standard;
   uint16_t pm10_env, pm25_env, pm100_env;
@@ -48,7 +48,7 @@ struct pms5003data {
   uint16_t unused;
   uint16_t checksum;
 };
-struct pms5003data data;
+struct pms7003data data;
 
 DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 LiquidCrystal_I2C lcd(0x27,20,4);
@@ -80,6 +80,19 @@ void loop() {
   check_MQ135();
   check_DHT11();
   check_PMS();
+  if (tempC > 40 && pm10State == pm25State == pm1State == 3 && fireAlert == 0){
+    send_SMS("Fire detected!");
+    fireAlert = 1;
+  }
+  else fireAlert == 0;
+
+  if ( pm25State == 3 && airAlert == 0){
+    send_SMS("Air quantity: BAD");
+    airAlert = 1;
+  }
+  if (pm25State == 2 && airAlert == 1 ) 
+    airAlert == 0;
+
   server.handleClient();
 }
 void lcd_Init(){
@@ -96,12 +109,12 @@ void lcd_Init(){
 void wifi_Init(){
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Initializing....");
+  lcd.print("Initializing...");
   delay(1000);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED);
   lcd.setCursor(0,1);
-  lcd.print("WiFi connected..!");
+  lcd.print("WiFi connected...!");
   lcd.setCursor(0,2);
   lcd.print("IP:");
   lcd.print(WiFi.localIP());
@@ -110,7 +123,7 @@ void wifi_Init(){
 }
 
 void GSM_Init(){
-  Serial.println("AT+CMGF=1"); // Configuring TEXT mode
+  Serial.println("AT+CMGF=1");  // Configuring TEXT mode
   delay(100);
   Serial.println("AT&W");
   delay(100);
@@ -135,20 +148,20 @@ void server_Init(){
 }
 
 void handle_OnConnect(){
-  server.send(200, "text/html", SendHTML(data.pm10_env, data.pm25_env, data.pm100_env));
+  server.send(200, "text/html", SendHTML(data.pm10_env, data.pm25_env, data.pm100_env, tempC, humi));
 }
 
 void handle_NotFound(){
   server.send(404, "text/plain", "Not found");
 }
 
-String SendHTML(int val1, int val2, int val3){
+String SendHTML(int pm1, int pm25, int pm10, float tempC, float humi){
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<title>Wireless Weather Station</title>\n";
+  ptr += "<title>Air monitoring</title>\n";
   ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
-  ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr += "body{margin-top: 24px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr += "p {font-size: 20px;color: #444444;margin-bottom: 10px;}\n";
   ptr += "</style>\n";
   ptr += "<script>\n";
   ptr += "setInterval(loadDoc,1000);\n";
@@ -165,19 +178,33 @@ String SendHTML(int val1, int val2, int val3){
   ptr += "</head>\n";
   ptr += "<body>\n";
   ptr += "<div id=\"webpage\">\n";
-  ptr += "<h1>Particulate Matter Monitoring</h1>\n";
+
+  ptr += "<div style=\" display: inline; \">";
+  ptr += "<p style=\"font-size: 15px; \">Sinh Viên: Lê Văn Dũng</p>";
+  ptr += "<p style=\"font-size: 15px; \">MSSV: 1912950</p>";
+  ptr += "</div>";
+  ptr += "<h1>Air Monitoring</h1>\n";
  
+
   ptr += "<p>PM1.0: ";
-  ptr += val1;
+  ptr += pm1;
   ptr += " ug/m3</p>";
  
   ptr += "<p>PM2.5: ";
-  ptr += val2;
+  ptr += pm25;
   ptr += " ug/m3</p>";
  
   ptr += "<p>PM10: ";
-  ptr += val3;
+  ptr += pm10;
   ptr += " ug/m3</p>";
+
+
+  ptr += "<p>Temp: ";
+  ptr += tempC;
+  ptr += " °C</p>";
+  ptr += "<p>Humi: ";
+  ptr += humi;
+  ptr += "% </p>";
  
   ptr += "</div>\n";
   ptr += "</body>\n";
@@ -187,18 +214,18 @@ String SendHTML(int val1, int val2, int val3){
 
 
 void check_MQ135(){
-  if (!digitalRead(MQ135_D) && gasDetect == 0 && alert == 0)
+  if (!digitalRead(MQ135_D) && gasDetect == 0)
   {
     delay(20);
     if (!digitalRead(MQ135_D))
     {
       gasDetect = 1;
       digitalWrite(LED_BUILTIN, HIGH);
-      //ledState = 1;
+      ledState = 1;
       digitalWrite(BUZZER, HIGH);
-      //buzzerState = 1;
+      buzzerState = 1;
+      digitalWrite(RL2, HIGH);
       send_SMS("Gas detected!");
-      alert = 1;
     }
   }
 
@@ -209,27 +236,20 @@ void check_MQ135(){
     {
       gasDetect = 0;
       digitalWrite(LED_BUILTIN, LOW);
-      //ledState = 0;
+      ledState = 0;
       digitalWrite(BUZZER, LOW);
-      //buzzerState = 0;
-      alert = 0;
+      buzzerState = 0;
+      digitalWrite(RL2, LOW);
     }
   }
 }
 
 void check_DHT11(){
-  // read humidity
+
   humi  = dht_sensor.readHumidity();
-  // read temperature in Celsius
   tempC = dht_sensor.readTemperature();
-  // read temperature in Fahrenheit
   tempF = dht_sensor.readTemperature(true);
 
-  // // check whether the reading is successful or not
-  // if ( isnan(tempC) || isnan(tempF) || isnan(humi)) {
-  //   Serial.println("Failed to read from DHT sensor!");
-  // } 
-  // else {
     lcd.setCursor(0,0);
     lcd.print("T:");
     lcd.print(tempC);
@@ -241,43 +261,84 @@ void check_DHT11(){
     lcd.print(humi);
     lcd.print("%");
 
-    if (tempC >= 35 || humi > 75 && RL1State == 0){
+    if (tempC >= 35 || humi > 80 && RL1State == 0){
       digitalWrite(RL1, HIGH);
       RL1State = 1;
     }
-    if (tempC < 35 && humi < 75 && RL1State == 1){
+    if (tempC < 35 && humi < 80 && RL1State == 1){
       digitalWrite(RL1, LOW);
       RL1State = 0;
     }
-  // }
 }
 
 void check_PMS(){
   if(readPMSdata(&Serial2)) {
-    lcd.setCursor(0,1);
+
+    lcd.setCursor(1,1);
     lcd.print("PM1.0:");
-    lcd.setCursor(6,1); 
+    lcd.setCursor(7,1); 
     lcd.print("   ");
-    lcd.setCursor(6,1); 
+    lcd.setCursor(7,1); 
     lcd.print(data.pm10_env);
-    lcd.setCursor(9,1);
+    lcd.setCursor(10,1);
     lcd.print("ug/m3");
-    lcd.setCursor(0,2);   
+    lcd.setCursor(16,1);
+    if (data.pm10_env <= 12){
+      lcd.print("PER");
+      pm1State = 1;
+    }
+    else if (data.pm10_env > 35){
+      lcd.print("BAD");
+      pm1State = 3;
+    }
+    else {
+      lcd.print("MED");
+      pm1State = 2;
+    }
+
+    lcd.setCursor(1,2);   
     lcd.print("PM2.5:");
-    lcd.setCursor(6,2); 
+    lcd.setCursor(7,2); 
     lcd.print("   ");
-    lcd.setCursor(6,2); 
+    lcd.setCursor(7,2); 
     lcd.print(data.pm25_env);
-    lcd.setCursor(9,2);
+    lcd.setCursor(10,2);
     lcd.print("ug/m3");
-    lcd.setCursor(0,3);   
+    lcd.setCursor(16,2);
+    if (data.pm25_env <= 12){
+      lcd.print("PER");
+      pm25State = 1;
+    }
+    else if (data.pm25_env > 35){
+      lcd.print("BAD");
+      pm25State = 3;
+    }
+    else {
+      lcd.print("MED");
+      pm25State = 2;
+    }
+
+    lcd.setCursor(1,3);   
     lcd.print("PM10 :"); 
-    lcd.setCursor(6,3); 
+    lcd.setCursor(7,3); 
     lcd.print("   ");
-    lcd.setCursor(6,3);
+    lcd.setCursor(7,3);
     lcd.print(data.pm100_env);
-    lcd.setCursor(9,3);
+    lcd.setCursor(10,3);
     lcd.print("ug/m3");
+    lcd.setCursor(16,3);
+    if (data.pm100_env <= 54){
+      lcd.print("PER");
+      pm10State = 1;
+    }
+    else if (data.pm100_env > 154){
+      lcd.print("BAD");
+      pm10State = 3;
+    }
+    else{
+      lcd.print("MED");
+      pm10State = 2;
+    }
   }
 }
 
